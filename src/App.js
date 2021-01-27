7275e28a6f93e9b594f4a19438daeb396e57d7dc
CapUlDis <capuldis@gmail.com>
2021-01-27 09:20:43 +0300

    Some experiments with backend requests.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { SplitLayout, SplitCol, Root, View, Panel, PanelHeader } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 
-const App = () => {
 
-	useEffect(() => {
-		console.log(window.location.search.slice(1))
-	});
+const App = () => {
+	const [activeView, setActiveView] = useState(null);
+
+	// useEffect(() => {
+	// 	const http = axios.create({
+	// 		headers: {
+	// 		  	// Прикрепляем заголовок, отвечающий за параметры запуска.
+	// 			Authorization: `Bearer ${window.location.search.slice(1)}`,
+	// 		},
+	// 	});
+
+	// 	console.log(window.location.search.slice(1));
+	// 	(async () => {
+	// 		// await fetch('http://localhost:3000/auth', {
+	// 		// 	headers: { Authorization: `Bearer ${window.location.search.slice(1)}` }
+	// 		// });
+	// 		await http.get('http://localhost:3000/');
+	// 	})();
+	// });
 
 	return (
 		<SplitLayout>
 			<SplitCol>
-				<Root activeView='test'>
+				<Root activeView={activeView}>
 					<View id='test' activePanel='test'>
 						<Panel id='test'>
 							<PanelHeader>
