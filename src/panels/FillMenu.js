9669c71f96151d1796af07ff00ec05a5c35cf888
CapUlDis <capuldis@gmail.com>
2021-03-16 15:57:12 +0300

    Styles for FillMenu panel.

--- src/panels/FillMenu.js
+++ src/panels/FillMenu.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React,  { useState }from 'react';
 import cloneDeep from 'lodash-es/cloneDeep';
-import { Panel, PanelHeader, PanelHeaderButton, FixedLayout, Div, Group, Header, Link, Cell, List, CellButton, Avatar, Banner, usePlatform } from '@vkontakte/vkui';
+import { Panel, PanelHeader, PanelHeaderButton, FixedLayout, Div, Group, Header, Text, Link, Cell, List, CellButton, Avatar, Separator, usePlatform } from '@vkontakte/vkui';
 import { Icon28EditOutline } from '@vkontakte/icons';
 import { Icon24PenOutline } from '@vkontakte/icons';
-import { Icon20Add } from '@vkontakte/icons';
+import {   } from '@vkontakte/icons';
 import { Icon24ViewOutline } from '@vkontakte/icons';
 import { Icon24MoreHorizontal } from '@vkontakte/icons';
+import { Icon24AddOutline } from '@vkontakte/icons';
 
 import API from '../utils/API';
-import foodVk from './components/img/foodvk.svg';
+import './FillMenu.css';
 import orderArray from '../utils/orderArray';
 import { useRouter } from '@happysanta/router';
 import { MODAL_PAGE_POSITION, POPOUT_EDIT_DELETE_POSITION, PAGE_EDIT_CATEGORIES, PAGE_MENU } from '../router';
@@ -16,15 +17,18 @@ import { MODAL_PAGE_POSITION, POPOUT_EDIT_DELETE_POSITION, PAGE_EDIT_CATEGORIES,
 
 const FillMenu = ({ id, desktop, group, setGroup, setPosition, setCategories, setCatOrder }) => {
   const router = useRouter();
+  const platform = usePlatform();
 
   return (
     <Panel id={id}>
-      <PanelHeader separator={false}
-        left={!desktop && <PanelHeaderButton onClick={() => {
-          const cloneGroup = cloneDeep(group);
-          setCategories(cloneGroup.Categories);
-          setCatOrder(cloneGroup.catOrder);
-          router.pushPage(PAGE_EDIT_CATEGORIES);
+      <PanelHeader 
+        left={desktop
+          ? undefined
+          : <PanelHeaderButton onClick={() => {
+            const cloneGroup = cloneDeep(group);
+            setCategories(cloneGroup.Categories);
+            setCatOrder(cloneGroup.catOrder);
+            router.pushPage(PAGE_EDIT_CATEGORIES);
           }}>
             <Icon28EditOutline />
           </PanelHeaderButton>
@@ -32,70 +36,60 @@ const FillMenu = ({ id, desktop, group, setGroup, setPosition, setCategories, se
       >
         Ваше Меню
       </PanelHeader>
-      <Banner
-        before={<img src={foodVk} />}
-        text='Добавьте ссылку на страницу заведения в Еде ВКонтакте'
-      />
-      <CellButton onClick={() => console.log(group)}>Консоль групп</CellButton>
-      {group.Categories.map((category, catIndex) => {
-        return (
-          <Group key={'cat' + category.id} header={
-            <Header mode="primary"
-              indicator={category.Positions ? category.Positions.length : 0}
-              aside={
-                <Link onClick={() => {
-                  setPosition({ categoryId: category.id });
-                  return router.pushModal(MODAL_PAGE_POSITION);
-                }}>
-                  {desktop ? 'Добавить блюдо' : <Icon20Add />}
-                </Link>
-              }
-            >
-              {category.title}
-            </Header>
-          }>
-            <List>
-              {category.Positions && category.Positions.map(position => {
-                return (
-                  <Cell draggable
-                    key={'pos' + position.id}
-                    before={<Avatar mode='app' src={position.imageUrl} />}
-                    indicator={<Icon24MoreHorizontal onClick={() => {
-                      setPosition(position);
-                      return router.pushPopup(POPOUT_EDIT_DELETE_POSITION);
-                    }}/>}
-                    description={position.price + ' ₽'}
-                    onDragFinish={async ({ from, to }) => {
-                      const cloneGroup = cloneDeep(group);
-                      cloneGroup.Categories[catIndex].posOrder.splice(from, 1);
-                      cloneGroup.Categories[catIndex].posOrder.splice(to, 0, group.Categories[catIndex].posOrder[from]);
-                      cloneGroup.Categories[catIndex].Positions = orderArray(cloneGroup.Categories[catIndex].Positions, cloneGroup.Categories[catIndex].posOrder, 'id');
-                      setGroup(cloneGroup);
-                      try {
-                        const cloneGroup = cloneDeep(group);
-                        cloneGroup.Categories[catIndex].posOrder.splice(from, 1);
-                        cloneGroup.Categories[catIndex].posOrder.splice(to, 0, group.Categories[catIndex].posOrder[from]);
-                        cloneGroup.Categories[catIndex].Positions = orderArray(cloneGroup.Categories[catIndex].Positions, cloneGroup.Categories[catIndex].posOrder, 'id');
-
-                        await API.patch(`/categories/${category.id}`, { posOrder: cloneGroup.Categories[catIndex].posOrder })
-                        setGroup(cloneGroup);
-                      } catch (err) {
-                        console.log(err);
-                      }
-                    }}
-                  >
-                    {position.title}
-                  </Cell>
-                );
-              })}
-            </List>
-          </Group>
-        )
-      })}
-      <FixedLayout vertical='bottom'>
-        <Div>
-          {desktop && <CellButton before={<Icon24PenOutline />}>Изменить категории</CellButton>}
-          <CellButton before={<Icon24ViewOutline/>} onClick={() => router.pushPage(PAGE_MENU)}>Предпросмотр меню</CellButton>
+      {/* <CellButton onClick={() => console.log(platform)}>Консоль platform</CellButton> */}
+      <Group style={{ paddingBottom: '72px' }}>
+        {group.Categories.map((category, catIndex) =>
+            <Group key={'cat' + category.id}
+              header={
+              <Header mode="primary"
+                indicator={category.Positions ? category.Positions.length : 0}
+                aside={
+                  <Link onClick={() => {
+                    setPosition({ categoryId: category.id });
+                    return router.pushModal(MODAL_PAGE_POSITION);
+                  }}>
+                    {desktop ? 'Добавить блюдо' : <Icon24AddOutline  />}
+                  </Link>
+                }
+              >
+                {category.title}
+              </Header>
+            }>
+              <List>
+                {category.Positions && category.Positions.map(position =>
+                    <Cell draggable
+                      key={'pos' + position.id}
+                      before={<Avatar mode='app' src={position.imageUrl} />}
+                      indicator={<Icon24MoreHorizontal onClick={() => {
+                        setPosition(position);
+                        return router.pushPopup(POPOUT_EDIT_DELETE_POSITION);
+                      }}/>}
+                      description={position.price + ' ₽'}
+                      onDragFinish={async ({ from, to }) => {
+                        try {
+                          const cloneGroup = cloneDeep(group);
+                          cloneGroup.Categories[catIndex].posOrder.splice(from, 1);
+                          cloneGroup.Categories[catIndex].posOrder.splice(to, 0, group.Categories[catIndex].posOrder[from]);
+                          cloneGroup.Categories[catIndex].Positions = orderArray(cloneGroup.Categories[catIndex].Positions, cloneGroup.Categories[catIndex].posOrder, 'id');
+                          await API.patch(`/categories/${category.id}`, { posOrder: cloneGroup.Categories[catIndex].posOrder })
+                          setGroup(cloneGroup);
+                        } catch (err) {
+                          console.log(err);
+                        }
+                      }}
+                    >
+                      {position.title}
+                    </Cell>
+                )}
+              </List>
+            </Group>
+        )}
+      </Group>
+      <FixedLayout vertical='bottom' filled>
+        <Separator wide />
+        <Div className={desktop ? 'footer-desktop' : undefined}>
+          {desktop && <CellButton className="footer-desktop__cell-button" before={<Icon24PenOutline />}>Изменить категории</CellButton>}
+          <CellButton className="footer-desktop__cell-button" before={<Icon24ViewOutline/>} onClick={() => router.pushPage(PAGE_MENU)}>Предпросмотр меню</CellButton>
         </Div>
       </FixedLayout>
     </Panel>
