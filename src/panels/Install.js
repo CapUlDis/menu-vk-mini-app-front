e5af2eed574c597509492a999de995d9f01c8dc0
CapUlDis <capuldis@gmail.com>
2021-04-06 12:59:31 +0300

    Add snackbarError to Install panel.

--- src/panels/Install.js
+++ src/panels/Install.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import bridge from "@vkontakte/vk-bridge";
 import { Panel, Placeholder, FixedLayout, Div, Button } from '@vkontakte/vkui';
 
+import SnackbarError from '../popouts/SnackbarError';
 import menu from './components/img/menu.svg';
 import './Install.css';
 
 
 const Install = ({ id, desktop }) => {
+  const [snackbarError, setSnackbarError] = useState(null);
 
   const addMenuToCommunity = async () => {
     try {
       await bridge.send("VKWebAppAddToCommunity");
+
+      return;
     } catch (err) {
-      console.log(err);
+      if (err.error_data && err.error_data.error_code === 1) {
+        setSnackbarError(
+          <SnackbarError setSnackbarError={setSnackbarError}>
+            Проблемы с получением данных от сервера. Проверьте интернет-соединение.
+          </SnackbarError>
+        );
+      }
+
       return;
     }
 
@@ -24,19 +35,20 @@ const Install = ({ id, desktop }) => {
         icon={<img src={menu} />}
         header='Меню заведения'
         stretched
-        action={desktop && <Button size="m" onClick={addMenuToCommunity}>Установить в сообщество</Button>}
+        action={desktop && <Button size="l" onClick={addMenuToCommunity}>Установить в сообщество</Button>}
       >
         <p className="placeholder__text">Описание приложения. <br />Выберите категории, которые представлены в вашем заведении. Позже вы сможете их изменить или создать новые.</p>
       </Placeholder>
-      <FixedLayout vertical='bottom'>
-        {!desktop &&
+      {!desktop &&
+        <FixedLayout vertical='bottom'>
           <Div>
             <Button size='l' stretched onClick={addMenuToCommunity}>
               Установить в сообщество
             </Button>
           </Div>
-        }
-      </FixedLayout>
+        </FixedLayout>
+      }
+      {snackbarError}
     </Panel>
   )
 };
