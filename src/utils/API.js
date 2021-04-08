f337aa929d6a2099737fde4f926796d8ee59f420
CapUlDis <capuldis@gmail.com>
2021-04-08 10:17:48 +0300

    Change API url and vk_group_id in vk-hosting-config.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
+<<<<<<< HEAD
   baseURL: "https://menu.dev1.hsstore.ru/api/",
+=======
+  baseURL: "http://menu.dev1.hsstore.ru/api/",
+>>>>>>> Change API url and vk_group_id in vk-hosting-config.
   responseType: "json",
   headers: { Authorization: `Bearer ${window.location.search.slice(1)}` },
 });
