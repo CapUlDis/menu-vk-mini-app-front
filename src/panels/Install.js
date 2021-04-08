78754529d1f7ece07154633645d3e4625fd252f6
CapUlDis <capuldis@gmail.com>
2021-04-08 17:10:03 +0300

    Add link to community in Install panel.

--- src/panels/Install.js
+++ src/panels/Install.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import bridge from "@vkontakte/vk-bridge";
-import { Panel, Placeholder, FixedLayout, Div, Button } from '@vkontakte/vkui';
+import { Panel, Placeholder, FixedLayout, Div, Button, Link } from '@vkontakte/vkui';
 
 import SnackbarError from '../popouts/SnackbarError';
 import menu from './components/img/menu.svg';
@@ -9,12 +9,13 @@ import './Install.css';
 
 const Install = ({ id, desktop }) => {
   const [snackbarError, setSnackbarError] = useState(null);
+  const [link, setLink] = useState(false);
 
   const addMenuToCommunity = async () => {
     try {
-      await bridge.send("VKWebAppAddToCommunity");
+      const response = await bridge.send("VKWebAppAddToCommunity");
 
-      return;
+      return setLink(`https://vk.com/public${response.group_id}`);
     } catch (err) {
       if (err.error_data && err.error_data.error_code === 1) {
         setSnackbarError(
@@ -31,14 +32,18 @@ const Install = ({ id, desktop }) => {
 
   return (
     <Panel id={id}>
-      <Placeholder
+      <Placeholder className="placeholder"
         icon={<img src={menu} />}
         header='Меню заведения'
-        stretched
         action={desktop && <Button size="l" onClick={addMenuToCommunity}>Установить в сообщество</Button>}
       >
         <p className="placeholder__text">Описание приложения. <br />Выберите категории, которые представлены в вашем заведении. Позже вы сможете их изменить или создать новые.</p>
       </Placeholder>
+      {link &&
+        <Div className="link">
+          <Link href={link} target="_blank">Перейти в сообщество для запуска приложения</Link>
+        </Div>
+      }
       {!desktop &&
         <FixedLayout vertical='bottom'>
           <Div>
