762d1a2f82c605f46605b045c7a4591fde1a99bf
CapUlDis <capuldis@gmail.com>
2021-05-27 15:19:19 +0300

    Fix theme detecting.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from "react";
 import { BridgePlus } from "@happysanta/bridge-plus";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
-import { ConfigProvider, AdaptivityProvider, WebviewType, SizeType, VKCOM, ViewWidth, Scheme } from '@vkontakte/vkui';
+import { ConfigProvider, AdaptivityProvider, WebviewType, SizeType, VKCOM, ViewWidth } from '@vkontakte/vkui';
 import { RouterContext } from '@happysanta/router';
 import { router } from './router';
 import App from "./App";
@@ -18,13 +18,28 @@ const isDesktop = BridgePlus.getStartParams().isDesktop();
 
 bridge.subscribe(({ detail: { type, data }}) => {
   if (type === 'VKWebAppUpdateConfig' && !isDesktop) {
-    const schemeAttribute = document.createAttribute('scheme');
-    schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-    document.body.attributes.setNamedItem(schemeAttribute);
+    let scheme = (data.scheme === 'client_dark' || data.scheme === 'space_gray')
+      ? 'space_gray'
+      : 'bright_light';
+    
+    document.querySelector('body').setAttribute('scheme', scheme);
+
+    let status_bar_style = 'dark';
+    let action_bar_color = '#FFFFFF';
+
+    if (scheme === 'space_gray') {
+      status_bar_style = 'light';
+      action_bar_color = '#19191A';
+    }
+
+    if (bridge.supports('VKWebAppSetViewSettings')) {
+      bridge.send('VKWebAppSetViewSettings', {
+        status_bar_style,
+        action_bar_color
+      });
+    }
   }
 });
-
-
 
 ReactDOM.render(
 	<RouterContext.Provider value={router}>
