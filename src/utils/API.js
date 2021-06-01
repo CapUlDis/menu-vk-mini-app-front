567e416fe62348779b22b2908c6b2228ca125a6c
CapUlDis <capuldis@gmail.com>
2021-06-01 11:22:25 +0300

    API.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://menu.dev1.hsstore.ru/api/",
   responseType: "json",
   headers: { Authorization: `Bearer ${window.location.search.slice(1)}` },
 });
